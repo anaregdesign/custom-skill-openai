8e0e783c476dc396746da4f4e828f4723713dc9d
piroyoung <piroyoung@users.noreply.github.com>
2023-05-27 20:14:40 +0900

    init

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,53 @@
-package cmd
+package main
+
+import (
+	"github.com/anaregdesign/custom-skill-builder/service"
+	"github.com/anaregdesign/custom-skill-builder/skill"
+	"github.com/anaregdesign/custom-skill-openai/openai"
+	"os"
+)
 
 func main() {
-	
+
+	resourceName := os.Getenv("AOAI_RESOURCE_NAME")
+	deploymentName := os.Getenv("AOAI_DEPLOYMENT_NAME")
+	apiVersion := os.Getenv("AOAI_API_VERSION")
+	apiKey := os.Getenv("AOAI_API_KEY")
+
+	client := openai.New(resourceName, deploymentName, apiVersion, apiKey)
+
+	embeddingSkill := skill.NewSkill(client.Embed)
+	summarizeJaSkill := skill.NewSkill(client.SummarizeJA)
+	summarizeEnSkill := skill.NewSkill(client.SummarizeEN)
+	summarizeZhSkill := skill.NewSkill(client.SummarizeZH)
+	summarizeKoSkill := skill.NewSkill(client.SummarizeKO)
+	summarizeFrSkill := skill.NewSkill(client.SummarizeFR)
+	summarizeItSkill := skill.NewSkill(client.SummarizeIT)
+	summarizeEsSkill := skill.NewSkill(client.SummarizeES)
+	summarizePtSkill := skill.NewSkill(client.SummarizePT)
+	summarizeRuSkill := skill.NewSkill(client.SummarizeRU)
+	summarizeTrSkill := skill.NewSkill(client.SummarizeTR)
+	summarizeViSkill := skill.NewSkill(client.SummarizeVI)
+	summarizeIdSkill := skill.NewSkill(client.SummarizeID)
+	summarizeThSkill := skill.NewSkill(client.SummarizeTH)
+
+	book := skill.NewBook()
+	book.Register("embed", embeddingSkill.Flatten())
+	book.Register("summarize_ja", summarizeJaSkill.Flatten())
+	book.Register("summarize_en", summarizeEnSkill.Flatten())
+	book.Register("summarize_zh", summarizeZhSkill.Flatten())
+	book.Register("summarize_ko", summarizeKoSkill.Flatten())
+	book.Register("summarize_fr", summarizeFrSkill.Flatten())
+	book.Register("summarize_it", summarizeItSkill.Flatten())
+	book.Register("summarize_es", summarizeEsSkill.Flatten())
+	book.Register("summarize_pt", summarizePtSkill.Flatten())
+	book.Register("summarize_ru", summarizeRuSkill.Flatten())
+	book.Register("summarize_tr", summarizeTrSkill.Flatten())
+	book.Register("summarize_vi", summarizeViSkill.Flatten())
+	book.Register("summarize_id", summarizeIdSkill.Flatten())
+	book.Register("summarize_th", summarizeThSkill.Flatten())
+
+	svc := service.NewCustomSkillService(book)
+	svc.Run()
+
 }
--- openai/openai.go
+++ openai/openai.go
@@ -3,12 +3,19 @@ package openai
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/anaregdesign/custom-skill-builder/model"
-	aoai "github.com/anaregdesign/go-aoai"
+	"github.com/anaregdesign/go-aoai"
 )
 
 type Client struct {
 	client *aoai.AzureOpenAI
+}
+
+func New(resourceName string, deproymentName string, apiVersion string, apiKey string) *Client {
+	return &Client{
+		client: aoai.New(resourceName, deproymentName, apiVersion, apiKey),
+	}
 }
 
 func (c *Client) Embed(ctx context.Context, d *model.Data[string]) (*model.Data[[]float64], error) {
@@ -24,14 +31,82 @@ func (c *Client) Embed(ctx context.Context, d *model.Data[string]) (*model.Data[
 	return &model.Data[[]float64]{Output: response.Data[0].Embedding}, nil
 }
 
-func (c *Client) Summarize(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
-	system := "You are the multilingual speaker. Summarize given text with given language."
-	summary, err := c.chatCompletion(ctx, system, d.Input, 0.1, 0.9)
+func (c *Client) summarize(ctx context.Context, d *model.Data[string], languageCode string) (*model.Data[string], error) {
+	system := fmt.Sprintf("You are the multilingual assistant. Please summarize given text with %s.", languageCode)
+	summary, err := c.chatCompletion(ctx, system, d.Input, 0.1, 0.5)
 	if err != nil {
 		return nil, err
 	}
 
 	return &model.Data[string]{Output: summary}, nil
+}
+
+func (c *Client) SummarizeJA(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "ja")
+}
+
+func (c *Client) SummarizeEN(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "en")
+}
+
+func (c *Client) SummarizeZH(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "zh")
+}
+
+func (c *Client) SummarizeKO(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "ko")
+}
+
+func (c *Client) SummarizeFR(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "fr")
+}
+
+func (c *Client) SummarizeDE(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "de")
+}
+
+func (c *Client) SummarizeES(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "es")
+}
+
+func (c *Client) SummarizePT(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "pt")
+}
+
+func (c *Client) SummarizeIT(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "it")
+}
+
+func (c *Client) SummarizeNL(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "nl")
+}
+
+func (c *Client) SummarizePL(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "pl")
+}
+
+func (c *Client) SummarizeRU(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "ru")
+}
+
+func (c *Client) SummarizeTR(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "tr")
+}
+
+func (c *Client) SummarizeVI(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "vi")
+}
+
+func (c *Client) SummarizeAR(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "ar")
+}
+
+func (c *Client) SummarizeID(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "id")
+}
+
+func (c *Client) SummarizeTH(ctx context.Context, d *model.Data[string]) (*model.Data[string], error) {
+	return c.summarize(ctx, d, "th")
 }
 
 func (c *Client) chatCompletion(ctx context.Context, system string, prompt string, t float64, p float64) (string, error) {
